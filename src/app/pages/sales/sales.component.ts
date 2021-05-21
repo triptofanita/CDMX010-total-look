032734018ffcae2fc31a1e5a2626409e612a4a3c
osiris <wendyosi25@gmail.com>
2021-05-21 10:28:54 -0500

    sales modified

--- src/app/pages/sales/sales.component.ts
+++ src/app/pages/sales/sales.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-sales',
   templateUrl: './sales.component.html',
   styleUrls: ['./sales.component.css']
 })
-export class SalesComponent implements OnInit {
+export class SalesComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  promociones: Observable <any[]>;
+  constructor(db: AngularFirestore) {
+    this.promociones=db.collection('promociones').valueChanges();
   }
 
 }
